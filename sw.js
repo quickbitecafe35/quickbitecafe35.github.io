0e08dc7cf4f28c703dedb420d9d7d03628fed609
quickbitecafe35 <quickbitecafe35@gmail.com>
2026-02-09 21:15:31 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-self.addEventListener("install", () => {
-  self.skipWaiting();
+const cacheName = 'quickbite-v1';
+const assets = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
